034375d819783bf3a77631c504297a932591e889
SOHDA31 <sohda151298@gmail.com>
2021-09-27 20:35:13 +0530

    changes

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-outputDiv.innerHTML = "HI! Write something here";
+outputDiv.innerHTML = "Your output sentence in banana language will appear here";
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
 function getTranslationURL(text) {
-    return serverURL + "?" + "text" + text
+    return serverURL + "?" + "text=" + text;
 }
 
 function errorHandler(error) {
@@ -25,8 +25,8 @@ function clickHandler() {
 
 //calling server for processing
     fetch(getTranslationURL(inputText))
-        .then(response => response.json())
-        .then(json => {
+        .then((response) => response.json())
+        .then((json) => {
             var translatedText = json.contents.translated;
             outputDiv.innerText = translatedText;
 
