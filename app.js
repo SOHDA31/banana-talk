1fd52e5144bbbf461ab8cc6113522c2297e0b92c
SOHDA31 <sohda151298@gmail.com>
2021-09-27 20:21:06 +0530

    api call added

--- app.js
+++ app.js
@@ -4,9 +4,36 @@ var outputDiv = document.querySelector("#output");
 
 outputDiv.innerHTML = "HI! Write something here";
 
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text" + text
+}
+
+function errorHandler(error) {
+    console.log("error occured", error)
+    alert("something wrong with server! try again after sometime.")
+}
+
+
+
 
 function clickHandler() {
-    outputDiv.innerText = "gfeiyfygigifughuguh " + textInput.value;
+//    outputDiv.innerText = "gfeiyfygigifughuguh " + textInput.value;
+
+    var inputText = textInput.value; //taking the input
+
+//calling server for processing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+
+        })
+        .catch(errorHandler)
+
+
 };
 
 btnTranslate.addEventListener("click", clickHandler)
