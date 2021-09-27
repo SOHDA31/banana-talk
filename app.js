e87ca31a75d50f2d425a9af66cde9cfc874bd210
SOHDA31 <sohda151298@gmail.com>
2021-09-27 17:19:02 +0530

    input output process

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
-
-
+outputDiv.innerHTML = "HI! Write something here";
 
 
 function clickHandler() {
-    console.log("input", textInput.value);
+    outputDiv.innerText = "gfeiyfygigifughuguh " + textInput.value;
 };
 
 btnTranslate.addEventListener("click", clickHandler)
